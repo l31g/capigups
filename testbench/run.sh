a31ab8ba14d02158801bdc7b2ad60624720d1318
Luis E. P <l31g@utexas.edu>
2015-11-30 20:02:22 -0600

    fixed run

--- testbench/run.sh
+++ testbench/run.sh
@@ -32,9 +32,7 @@ then
 	fi
 	# TODO needs to be fixed so it can take a "master" file
 	cd ./$DIR
-    vcs +v2k -F ../master ../$FILE #-full64
-	#vcs $SRC/lib/time $SRC/lib/lib* $SRC/libx/* $SRC/adder/*.v $SRC/ild/modrm_ild.v $SRC/ild/prefix_decoder.v $SRC/ild/prefix_3b.v $SRC/ild/len_dec2.v $SRC/ild/len_dec1.v $SRC/ild/len_bundle.v $SRC/ild/len_group.v $SRC/ild/buffer_ild.v $SRC/ild/toplevel_ild.v $SRC/barrel/bshfr_n_8.v $SRC/barrel/brotr_n_8.v $SRC/barrel/bshfl_n_8.v $SRC/barrel/brotl_n_8.v ../$FILE +v2k
-	#vcs $SRC/lib/time $SRC/lib/lib* $SRC/adder/pg*.v $SRC/adder/add32.v $SRC/adder/add5.v ../$FILE +v2k
+    	vcs +v2k -F ../master ../$FILE #-full64
 	./simv
 
 	echo
