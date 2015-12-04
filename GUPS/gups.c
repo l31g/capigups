78833a23b4bf8274cae2bd599caed5e4fe793956
kapil <kapil@utexas.edu>
2015-12-03 18:35:53 -0600

    cleaned up code

--- GUPS/gups.c
+++ GUPS/gups.c
@@ -19,7 +19,27 @@ typedef long long s64Int;
 typedef unsigned long long u64Int;
 #endif
 
-u64Int HPCC_starts(s64Int n);
+
+unsigned 
+rand256()
+{
+    static unsigned const limit = RAND_MAX - RAND_MAX % 256;
+    unsigned result = rand();
+    while ( result >= limit ) {
+        result = rand();
+    }
+    return result % 256;
+}
+
+unsigned long long
+rand64bits()
+{
+    unsigned long long results = 0ULL;
+    for ( int count = 8; count > 0; -- count ) {
+        results = 256U * results + rand256();
+    }
+    return results;
+}
 
 int main(int narg, char **arg)
 {
@@ -33,7 +53,9 @@ int main(int narg, char **arg)
   u64Int *table,*data,*send;
   u64Int ran,datum,procmask,nglobal,offset,nupdates;
   u64Int ilong,nexcess_long,nbad_long;
-
+  time_t t;	
+  srand((unsigned) time(&t));
+	
   /* command line args = N M chunk
      N = length of global table is 2^N
      M = # of update sets per proc
@@ -51,14 +73,8 @@ int main(int narg, char **arg)
   i = 1;
   while (i < nprocs) i *= 2;
   if (i != nprocs) {
-    if (me == 0) printf("Must run on power-of-2 procs\n");
+    //if (me == 0) printf("Must run on power-of-2 procs\n");
   }
-
-  /* nglobal = entire table
-     nlocal = size of my portion
-     nlocalm1 = local size - 1 (for index computation)
-     logtablelocal = log of table size I store
-     offset = starting index in global table of 1st entry in local table */
 
   logprocs = 0;
   while (1 << logprocs < nprocs) logprocs++;
@@ -88,18 +104,14 @@ int main(int narg, char **arg)
   for (i = 0; i < nlocal; i++) table[i] = i + offset;
 
 
-res = (r30 << 34) + (s30 << 4) + t4 
+ 
    
    		
   nupdates = (u64Int) nprocs * chunk * niterate;
-  ran = HPCC_starts(nupdates/nprocs*me);
+  
 
 
-  /* loop:
-       generate chunk random values per proc
-       communicate datums to correct processor via hypercube routing
-       use received values to update local table */
-
+ 
   maxndata = 0;
   maxnfinal = 0;
   nexcess = 0;
@@ -112,80 +124,27 @@ res = (r30 << 34) + (s30 << 4) + t4
 	  //  printf("Error opening file!\n");
     //exit(1);
 	//}
-	int mask_size = atoi(arg[4]);
-	int mask;
-	mask = (1 << (mask_size*4)) - 1;
+	u64Int mask_size = atoi(arg[4]);
+	u64Int mask;
+	u64Int constant;
+	constant = 1;
+	
+	mask = (constant << (mask_size*4)) - 1;
 	//printf ("%d,\n%d\n",mask,mask_size);
 	//mask = 3;
 		
-	time_t t;	
-	srand((unsigned) time(&t));
   for (iterate = 0; iterate < niterate; iterate++) {
     for (i = 0; i < chunk; i++) {
-     ran = rand();	
-  	//ran = HPCC_starts(nupdates/nprocs*me);
-	 ran = (ran << 1) ^ ((s64Int) ran < ZERO64B ? POLY : ZERO64B);
+     ran = rand64bits();//rand();
       ran = ran & mask  ;
       data[i] = ran;
 			//fprintf(file, "%016llx\n",ran);
-			printf("%016llx\n",ran);
+	  printf("%016llx\n",ran);
     }
     ndata = chunk;
 		//fclose(file);
 
-    for (j = 0; j < logprocs; j++) {
-      nkeep = nsend = 0;
-      ipartner = (1 << j) ^ me;
-      procmask = ((u64Int) 1) << (logtablelocal + j);
-      if (ipartner > me) {
-	for (i = 0; i < ndata; i++) {
-	  if (data[i] & procmask) send[nsend++] = data[i];
-	  else data[nkeep++] = data[i];
-	}
-      } else {
-	for (i = 0; i < ndata; i++) {
-	  if (data[i] & procmask) data[nkeep++] = data[i];
-	  else send[nsend++] = data[i];
-	}
-      }
-
-    }
-    if (ndata > chunk) nexcess += ndata - chunk;
-
-    for (i = 0; i < ndata; i++) {
-      datum = data[i];
-      index = datum & nlocalm1;
-      table[index] ^= datum;
-    }
-
-#ifdef CHECK
-    procmask = ((u64Int) (nprocs-1)) << logtablelocal;
-    for (i = 0; i < ndata; i++)
-      if ((data[i] & procmask) >> logtablelocal != me) nbad++;
-#endif
-  }
-
-
-  i = maxndata;
-  i = maxnfinal;
-  ilong = nexcess;
-  ilong = nbad;
-
-  nupdates = (u64Int) niterate * nprocs * chunk;
-  Gups = nupdates / t0 / 1.0e9;
-
-  if (me == 0) {
-    printf("Number of procs: %d\n",nprocs);
-    printf("Vector size: %lld\n",nglobal);
-    printf("Max datums during comm: %d\n",maxndata);
-    printf("Max datums after comm: %d\n",maxnfinal);
-    printf("Excess datums (frac): %lld (%g)\n",
-	   nexcess_long,(double) nexcess_long / nupdates);
-    printf("Bad locality count: %lld\n",nbad_long);
-    printf("Update time (secs): %9.3f\n",t0);
-    printf("Gups: %9.6f\n",Gups);
-  }
-
+  
   /* clean up */
 
   free(table);
@@ -195,38 +154,3 @@ res = (r30 << 34) + (s30 << 4) + t4
 
 
 
-u64Int HPCC_starts(s64Int n)
-{
-  int i, j;
-  u64Int m2[64];
-  u64Int temp, ran;
-
-  while (n < 0) n += PERIOD;
-  while (n > PERIOD) n -= PERIOD;
-  if (n == 0) return 0x1;
-
-  temp = 0x1;
-  for (i=0; i<64; i++) {
-    m2[i] = temp;
-    temp = (temp << 1) ^ ((s64Int) temp < 0 ? POLY : 0);
-    temp = (temp << 1) ^ ((s64Int) temp < 0 ? POLY : 0);
-  }
-
-  for (i=62; i>=0; i--)
-    if ((n >> i) & 1)
-      break;
-
-  ran = 0x2;
-  while (i > 0) {
-    temp = 0;
-    for (j=0; j<64; j++)
-      if ((ran >> j) & 1)
-        temp ^= m2[j];
-    ran = temp;
-    i -= 1;
-    if ((n >> i) & 1)
-      ran = (ran << 1) ^ ((s64Int) ran < 0 ? POLY : 0);
-  }
-
-  return ran;
-}
